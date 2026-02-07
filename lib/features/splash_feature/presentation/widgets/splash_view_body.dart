1720d217a2b12a2612c90badcee6687a5084135f
salma <zweensam@gmail.com>
2026-02-07 18:08:47 +0200

    add animation to splash view

--- lib/features/splash_feature/presentation/widgets/splash_view_body.dart
+++ lib/features/splash_feature/presentation/widgets/splash_view_body.dart
@@ -2,8 +2,47 @@ import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 import 'package:food_delivery_app/core/assets/app_images.dart';
 
-class SplashViewBody extends StatelessWidget {
+class SplashViewBody extends StatefulWidget {
   const SplashViewBody({super.key});
+
+  @override
+  State<SplashViewBody> createState() => _SplashViewBodyState();
+}
+
+class _SplashViewBodyState extends State<SplashViewBody>
+    with TickerProviderStateMixin {
+  late final AnimationController _topController;
+  late final AnimationController _logoController;
+  late Animation<double> fadeAnimation1;
+  late Animation<double> fadeAnimation2;
+
+  @override
+  void initState() {
+    super.initState();
+
+    _topController = AnimationController(
+      vsync: this,
+      duration: const Duration(seconds: 2),
+    );
+    _logoController =
+        AnimationController(vsync: this, duration: Duration(seconds: 4))
+          ..forward().whenComplete(() {
+            _topController.forward();
+          });
+
+    fadeAnimation1 = Tween<double>(begin: 0, end: 1).animate(_topController);
+
+    fadeAnimation2 = Tween<double>(begin: 0, end: 1).animate(
+      CurvedAnimation(parent: _logoController, curve: Curves.bounceInOut),
+    );
+  }
+
+  @override
+  void dispose() {
+    _topController.dispose();
+    _logoController.dispose();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -11,14 +50,23 @@ class SplashViewBody extends StatelessWidget {
       body: Column(
         mainAxisAlignment: MainAxisAlignment.spaceBetween,
         children: [
-          Row(
-            mainAxisAlignment: MainAxisAlignment.start,
-            children: [SvgPicture.asset(AppImages.a1)],
+          FadeTransition(
+            opacity: fadeAnimation1,
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.start,
+              children: [SvgPicture.asset(AppImages.a1)],
+            ),
           ),
-          Center(child: SvgPicture.asset(AppImages.logo)),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.end,
-            children: [SvgPicture.asset(AppImages.a2)],
+          FadeTransition(
+            opacity: fadeAnimation2,
+            child: SvgPicture.asset(AppImages.logo),
+          ),
+          FadeTransition(
+            opacity: fadeAnimation1,
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.end,
+              children: [SvgPicture.asset(AppImages.a2)],
+            ),
           ),
         ],
       ),
